8d5b9665774f26df6897e99d6e289e22f8b0b160
Chris CEO <chris@tuah.com>
2026-08-15 21:55:19 +0800

    Sync production to GitHub: 2026-08-15-2155

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* TUAH Service Worker v2 - cache-busted via BUILD_ID (auto-updates on every deploy) */
-const CACHE_NAME = "tuah-" + "a7CG8IlohycxjqX6BEFPZ";
+const CACHE_NAME = "tuah-" + "BRHvN3wJODB7ixKuMfm-b";
 const APP_SHELL = [
   "/icon-192.png",
   "/icon-512.png",
